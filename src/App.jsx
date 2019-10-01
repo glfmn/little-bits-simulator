90b0c6bd7f7da69b87d640991b806aab680c1e6b
Gwen Lofman <Gwen@Lofman.io>
2019-10-01 00:50:48 -0400

    Re-create application with yarn
    create-react-app was causing all sorts of errors on import of
    react-dnd; to circumvent the errors I re-generated the project
    with yarn and moved the .git repository and appropriate files from
    the old version.
    
    I don't know why this fixed the errors, but it did.  Webpack was
    failing to properly include the new dependencies, perhaps
    create-react-app is not intended for use with yarn?

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import Bits from './component/Bits';
 import Bargraph from './component/Bargraph';
 import Dimmer from './component/Dimmer';
 import Simulator from './Simulator'
+import { DndProvider } from 'react-dnd'
+import HTML5Backend from 'react-dnd-html5-backend'
 
 class App extends Component {
     render() {
     return (
         <div className="app">
             Little Bits Simulator
+            <DndProvider backend={HTML5Backend}>
             <main className="simulator">
                 <Simulator>
                     <Dimmer>
@@ -23,6 +26,7 @@ class App extends Component {
             <footer className="bit-bar">
                 <Bits bits={[<Bargraph />, <Dimmer />]}/>
             </footer>
+            </DndProvider>
         </div>
     );
     }
